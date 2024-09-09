7e7b22c43a0f218731d35f572d09161467d16d11
Jesimiel Marte <20221124@itla.edu.do>
2024-09-09 15:24:01 -0400

    new exercise

---
+++ 7kyu_js/get_middle.js
@@ -0,0 +1,18 @@
+function getMiddle(s)
+{
+  //Code goes here!
+  let iOdd = ((s.length - 1 )/ 2)
+  let iEven = (s.length/2) - 1
+  if (s.length === 1){
+    return s
+  }
+  else if (s.length % 2 === 0){
+    return s.slice(iEven, iEven + 2)
+  }
+  else {
+    return s.slice(iOdd, iOdd + 1)
+  }
+  
+}
+
+console.log(getMiddle('testing'))
